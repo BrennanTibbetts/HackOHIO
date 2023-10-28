bd54ffafb9aa0b8790df6a05714ff807f927b42a
JackieCSE <123091482+JackieCSE@users.noreply.github.com>
2023-10-28 14:16:58 -0400

    wheel spinning ez

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 
 function App() {
+  const [animationDuration, setAnimationDuration] = useState(5); // Initial animation duration
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="contained" color="primary" >
-            Hello
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ animationDuration: `${animationDuration}s` }}
+        />
+        <Button id="speedButton" onClick={() => setAnimationDuration(duration => duration * 0.8)}>
+          Increase Speed
         </Button>
       </header>
     </div>
   );
 }
+
 
 export default App;
