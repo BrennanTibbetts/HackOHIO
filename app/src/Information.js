a61788e1842b7e3ca519b2bda10b1ae248163f81
Brennan Tibbetts <brennan2049@gmail.com>
2023-10-28 20:28:23 -0400

    final commit

--- app/src/Information.js
+++ app/src/Information.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Information.css'; // Assuming you have a CSS file for styling
 import ButtonMeanings from './ButtonMeanings';
 
